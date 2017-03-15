2a2d68aa81fc578cf0b94517394ed9d4c3c0e4c4
Anthony Scinocco <scinocco.a@gmail.com>
2017-03-15 19:17:23 -0400

    quick implementation

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,36 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { TouchID } from 'ionic-native';
 import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor(public navCtrl: NavController) {
-    
+  constructor(public navCtrl: NavController, public touchId: TouchID) {}
+
+  public ngOnInit()
+  {
+    TouchID.isAvailable()
+      .then(res => {
+        console.log('Touch ID available');
+
+        TouchID.verifyFingerprint('Scan Your Finger')
+        .then(res => {
+          alert(res);
+        })
+        .catch(err => {
+          for (var key in err) {
+            alert(err[key]);
+          }
+        })
+
+      }, err => {
+        for (var key in err) {
+          alert(err[key]);
+        }
+      })
   }
 
 }
